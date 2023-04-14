b6e1eb9d061dd7af8bd5907c96629bb16c9fca2c
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-04-14 10:56:36 +0200

    deploy all 3 contracts of 01_Unstoppable

--- src/01_Unstoppable/UnstoppableVault.sol
+++ src/01_Unstoppable/UnstoppableVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "solmate/src/utils/FixedPointMathLib.sol";
 import "solmate/src/utils/ReentrancyGuard.sol";
--- test/Unstoppable.t.sol
+++ test/Unstoppable.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity 0.8.7;
 
 import "forge-std/Test.sol";
 
 import "../src/01_Unstoppable/OhDamnVulnerableToken.sol";
-import "../src/01_Unstoppable/ReceiverUnstoppable.sol";
+//import "../src/01_Unstoppable/ReceiverUnstoppable.sol";
 
 
